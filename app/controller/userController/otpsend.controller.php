9b5db8553d7a1f3834047d0da5c8af97e855ee7d
PinkuInraft <pinkuagarwal2831@gmail.com>
2025-04-22 21:58:39 +0530

    Signup view and action

---
+++ app/controller/userController/otpsend.controller.php
@@ -0,0 +1,43 @@
+<?php
+
+require './../vendor/autoload.php';
+require './../app/configs/dbh.php';
+
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+
+$name     = $_POST['name'];
+$email    = $_POST['email'];
+$password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+
+$otp = rand(100000, 999999);
+
+$_SESSION['otp']       = $otp;
+$_SESSION['user_data'] = [
+    'name'     => $name,
+    'email'    => $email,
+    'password' => $password,
+];
+
+$mail = new PHPMailer(true);
+
+try {
+    $mail->isSMTP();
+    $mail->Host       = 'smtp.gmail.com';
+    $mail->SMTPAuth   = true;
+    $mail->Username   = 'pinku.kumar@innoraft.com';
+    $mail->Password   = 'qzna ejtf plfk ihkw';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+    $mail->Port       = 465;
+
+    $mail->setFrom('pinku.kumar@innoraft.com', 'MailerPHP');
+    $mail->addAddress($email);
+    $mail->isHTML(true);
+    $mail->Subject = 'Your OTP Code';
+    $mail->Body    = "Your OTP is: $otp";
+
+    $mail->send();
+    echo "OTP sent successfully to $email";
+} catch (Exception $e) {
+    echo "Mailer Error: {$mail->ErrorInfo}";
+}
--- router.php
+++ router.php
@@ -7,5 +7,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         case '/':
             require_once './../app/views/index.php';
             break;
+
+        case '/register':
+            require_once './../app/views/auth/register.view.php';
+            break;
+    }
+}
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    switch ($uri) {
+        case '/otpsend':
+            require_once './../app/controller/userController/otpsend.controller.php';
+            break;
+
+        case '/otpverify':
+            require_once './../app/controller/userController/otpverify.controller.php';
+            break;
+
     }
 }
